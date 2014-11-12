e7e24773da1917d432c4abe40d9c3c399afc0b1e
projetosistemaOS <betto_fernandes@hotmail.com>
2014-11-12 08:32:16 -0200

    Atualização 12/11

--- SistemaOSMVC/Models/dbSistemaOS.Designer.cs
+++ SistemaOSMVC/Models/dbSistemaOS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\48376.robertof\documents\visual studio 2013\Projects\SistemaOSMVC\SistemaOSMVC\Models\dbSistemaOS.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Visual Studio 2013\Projects\SistemasOS-master\SistemaOSMVC\Models\dbSistemaOS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
